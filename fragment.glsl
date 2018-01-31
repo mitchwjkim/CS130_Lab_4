14db3cbe7745574e5e30ea8e4b5bf0a622c330d0
Mitchell Kim <mkim106@wch132-30.cs.ucr.edu>
2018-01-31 11:52:25 -0800

    updated FragColor.rgb and FragColor.a

--- fragment.glsl
+++ fragment.glsl
@@ -9,8 +9,8 @@ void main()
     vec3 normal = normalize(N);
 
     float intensity = max(dot(L, normal),0.0);
-   // gl_FragColor.rgb = intensity * gl_FrontMaterial.diffuse.rgb * gl_LightSource[0].diffuse.rgb + gl_FrontMaterial.ambient.rgb * gl_LightSource[0].ambient.rgb;
-   // gl_FragColor.a = 1.0;
+    gl_FragColor.rgb = intensity * gl_FrontMaterial.diffuse.rgb * gl_LightSource[0].diffuse.rgb + gl_FrontMaterial.ambient.rgb * gl_LightSource[0].ambient.rgb;
+    gl_FragColor.a = 1.0;
 
    vec4 tex_color = texture2D(tex, gl_TexCoord[0].st);
    gl_FragColor = texture2D(tex, gl_TexCoord[0].st);
